da5349bec84b523b256966d10e04c5f8e240cf49
S1pi <miikavsipila@gmail.com>
2024-12-16 16:02:05 +0200

    Added express-validator body import

--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {getMe, postLogin} from '../controllers/auth-controller.js';
 import {authenticateToken} from '../middlewares/authentication.js';
 import {validationErrorHandler} from '../middlewares/error-handler.js';
+import {body} from 'express-validator';
 
 const authRouter = express.Router();
 
